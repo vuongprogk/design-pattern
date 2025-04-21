cb4236a3868da2e3a5e732545b2f880a263d9694
Tran Duc Vuong <VuongTD6@fpt.com>
2025-04-21 10:27:37 +0700

    add sample console app

---
+++ Opentelemetry/TestOpenTelemetry/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+await DoWork();
+
+async Task DoWork()
+{
+    await StepOne();
+    await StepTwo();
+}
+
+async Task StepTwo()
+{
+    await Task.Delay(100);
+}
+
+async Task StepOne()
+{
+    await Task.Delay(1000);
+}
+
+Console.WriteLine("Done!");
